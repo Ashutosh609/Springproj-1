d35fdf0ccd59c1eb735f106a9c21a84cfb20a80b
Ashutosh609 <86567083+Ashutosh609@users.noreply.github.com>
2021-10-07 13:31:28 +0530

    Update StaticWebApplication.java

--- src/main/java/com/example/demo/StaticWebApplication.java
+++ src/main/java/com/example/demo/StaticWebApplication.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StaticWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StaticWebApplication.class, args);
+		ApplicationContext conn =SpringApplication.run(StaticWebApplication.class, args);
 	}
 
 }
